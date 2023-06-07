2daebb0b3aa3ee864093f956bf5d6e721c27d78e
B-Above <101487290+B-Above@users.noreply.github.com>
2023-06-07 21:39:59 +0800

    update time to proposal

--- main_DAO.sol
+++ main_DAO.sol
@@ -11,24 +11,32 @@ contract DAO {
         //bool hasVoted;
     }
 
-    struct Proposal {
-        address creator;
-        string description;
-        uint256 index;
-        //ProposalVote pv;
-    }
-
+struct Proposal {
+    address creator;
+    string description;
+    uint256 index;
+    uint256 startTime;  // 添加startTime字段
+    uint256 endTime;  // 添加endTime字段
+    //ProposalVote pv;
+}
+//存储成员信息的映射
     string public name;
     mapping(address => Member) public members;
+    // 存储提案投票合约的映射
     mapping(uint256 => ProposalVote) private pros;
     MyToken private MYTN;  // 创建MYTN的实例
+    // 提案列表
     Proposal[] public proposals;
+    // 供应量
     uint private supplynumber;
 
+ // 限制只有成员可以调用的修饰符
     modifier onlyMember() {
         require(members[msg.sender].exists, "Only members can call this function");
         _;
     }
+
+    //初始化合约的初始状态
 
     constructor(uint256 initialSupply,string memory _name){
         createToken(initialSupply);
@@ -36,7 +44,7 @@ contract DAO {
         name = _name;
     }
 
-
+// 创建代币实例
     function createToken(uint256 _number) private {
         MYTN = new MyToken(_number);  // 创建DAO token的实例并保存在MYTN中
     }
@@ -49,11 +57,13 @@ contract DAO {
         uint256 tokenAmount = 10*receivedEther;
         uint256 token_num = CheckTokenOfDAO();
         if (token_num < tokenAmount){
+            // 代币不足，将以太币退回
             payable(msg.sender).transfer(receivedEther);
             members[msg.sender] = Member(true, 0);
             string  memory message = "You are already a member, but token not enough, ETH is back.";
             return message;
         } else {
+            // 发送代币给成员
             MYTN.transfer(msg.sender,tokenAmount);
             members[msg.sender] = Member(true, tokenAmount);
             string  memory message = "You are a member now.";
@@ -62,6 +72,7 @@ contract DAO {
         
     }
 
+// 购买代币
     function buyToken() external payable returns (string memory) {
         require(members[msg.sender].exists, "You are no a member, please join DAO first");
         uint256 receivedEther = msg.value;
@@ -69,37 +80,49 @@ contract DAO {
         uint256 tokenAmount = 10*receivedEther;
         uint256 token_num = CheckTokenOfDAO();
         if (token_num < tokenAmount){
+            // 代币不足，将以太币退回
             payable(msg.sender).transfer(receivedEther);
             return "Token not enough, ETH is back.";
         } else {
+            // 发送代币给成员
             MYTN.transfer(msg.sender,tokenAmount);
             members[msg.sender].tokens += tokenAmount;
             return "Buy token successfully.";
         }
     }
-
+// 查询成员自身持有的代币数量
     function CheckTokenOfMe() public view onlyMember returns (uint256) {
         return MYTN.balanceOf(msg.sender);
     }
-
+//查询调用者（成员）在DAO合约中拥有的代币数量
     function CheckTokenOfDAO() public view returns (uint256) {
         return MYTN.balanceOf(address(this));
     }
-
+//查询DAO合约本身拥有的代币数量
     function CheckTokens() public view returns (uint256) {
         return members[msg.sender].tokens;
     }
+//创建提案
 
-    function createProposal(string memory description) external onlyMember {
-        uint256 id =  proposals.length;
-        proposals.push(Proposal({
+function createProposal(string memory description, uint256 duration) external onlyMember {
+    // require(startTime < endTime, "Invalid time range");
+
+    uint256 startTime = block.timestamp; // 当前时间作为开始时间
+    uint256 endTime = startTime + duration; // 开始时间加持续时间（单位秒）
+
+    uint256 id = proposals.length;
+    proposals.push(
+        Proposal({
             creator: msg.sender,
             description: description,
-            index: id
-            //pv: new ProposalVote()
-        }));
-        pros[id] = new ProposalVote();
-    }
+            index: id,
+            startTime: startTime,
+            endTime: endTime
+        })
+    );
+    pros[id] = new ProposalVote(startTime,endTime);
+}
+
     
     function vote(uint256 proposalIndex, bool support, uint256 vote_num) external onlyMember {
         require(proposalIndex < proposals.length, "Invalid proposal index");
@@ -108,12 +131,18 @@ contract DAO {
         prov.vote(vote_num,support,members[msg.sender].tokens,msg.sender);
     }
 
+
+//查询提案的投票结果
     function Proposalresult(uint proID) public view
             returns (uint support, uint againest, bool pass)
     {
         (support,againest,pass) = pros[proposals[proID].index].Proposalresult();
     }
 
+
+
+
+//调用者（成员）在指定提案中剩余的投票权重
     function CheckWeight(uint proIndex) public view
             returns (uint weight)
     {
@@ -122,10 +151,7 @@ contract DAO {
         weight = pros[proposal.index].RestVoteWeight(msg.sender);
     }
 
-    function transferTo (address to, uint number) public
-    {
-        payable(to).transfer(number);
-    }
+    
 
     receive() external payable{
         // 获取接收到的以太币数量
@@ -145,6 +171,7 @@ contract DAO {
         require(success, "Vote function call failed");
     }
 
+//这是一个receive函数，用于接收以太币支付
    
     fallback() external { x = 1; }
     uint x;
